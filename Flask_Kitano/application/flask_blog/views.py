2157a0594f5ebc6230dc9da03f5510be81e6eafa
Kitanomy <kitano.hiroki.gm@mynavi.jp>
2024-05-27 04:25:01 +0000

    template

--- Flask_Kitano/application/flask_blog/views.py
+++ Flask_Kitano/application/flask_blog/views.py
@@ -1,5 +1,7 @@
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+
 
 @app.route('/')
 def show_entries():
-    return "Hello World!"
+    return render_template('entries/index.html')
