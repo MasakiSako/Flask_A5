e7a2e1f89e75414f539a040416d0634f1b45302f
Kitanomy <kitano.hiroki.gm@mynavi.jp>
2024-05-28 00:59:16 +0000

    自動でリンクを作成

--- Flask_Kitano/application/flask_blog/views.py
+++ Flask_Kitano/application/flask_blog/views.py
@@ -5,12 +5,13 @@ from flask_blog import app
 @app.route('/')
 def show_entries():
     if not session.get('logged_in'):
-        return redirect('/login')
+        return redirect(url_for('login'))
     return render_template('entries/index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             flash('ユーザ名が異なります')
@@ -19,7 +20,7 @@ def login():
         else:
             session['logged_in'] = True
             flash('ログインしました')
-            return redirect('/')
+            return redirect(url_for('show_entries'))
     return render_template('login.html')
 
 
@@ -27,4 +28,4 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('ログアウトしました')
-    return redirect('/')
+    return redirect(url_for('show_entries'))
