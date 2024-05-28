ef340e4289663dcc567fe1f939382951777c270b
Itdesu <okumura.itto.xt@mynavi.jp>
2024-05-28 02:42:06 +0000

    Okumura: Update-calcSalary_App

--- Okumura/calcsalary_app/salary/views/views.py
+++ Okumura/calcsalary_app/salary/views/views.py
@@ -2,6 +2,14 @@ from flask import request, redirect, url_for, render_template, flash, session
 from salary import app
 
 # Readme: / にリクエストがあった時の処理
-@app.route("/")
+@app.route('/')
+def lp():
+    return redirect(url_for('input'))
+
+@app.route('/input')
 def input():
     return render_template('input.html')
+
+@app.route('/output')
+def output():
+    return render_template('output.html')
