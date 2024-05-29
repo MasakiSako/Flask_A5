b56de811c5d6b2262c2d635ad03d2f4ca86733a5
Itdesu <okumura.itto.xt@mynavi.jp>
2024-05-29 06:10:14 +0000

    Okumura: Create-Add-Entries

---
+++ Okumura/application/flask_blog/models/entries.py
@@ -0,0 +1,20 @@
+from flask_blog import db
+from datetime import datetime
+
+class Entry(db.Model):
+    # Do: SQL(CREATE TABLE)
+    __tablename__ = 'entries'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True)
+    text = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+
+    # Do: Default-Program(Created-Model) 
+    def __init__(self, title=None, text=None):
+        self.title = title
+        self.text = text
+        self.created_at = datetime.utcnow()
+
+    # Do: Export-Format(Read-Model)
+    def __repr__(self):
+        return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
---
+++ Okumura/application/flask_blog/views/entries.py
@@ -0,0 +1,34 @@
+from flask_blog.models.entries import Entry
+from flask_blog import db
+
+from flask import request, redirect, url_for, render_template, flash, session
+from flask_blog import app
+
+# Do: Create_Add-New-Entry(View)
+@ app.route('/')
+def show_entries():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    # Do: Read_get-data-from-table
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('entries/index.html', entries=entries)
+
+@app.route('/entries', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    
+    entry = Entry(
+        title = request.form['title'],
+        text = request.form['text']
+    )
+    db.session.add(entry)
+    db.session.commit()
+    flash('新しく記事が作成されました')
+    return redirect(url_for('show_entries'))
+
+@app.route('/entries/new', methods=["GET"])
+def new_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/new.html')
