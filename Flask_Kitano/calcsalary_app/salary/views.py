1a70432c5a391488806491d630756a773334b6fc
Kitanomy <kitano.hiroki.gm@mynavi.jp>
2024-05-29 01:10:54 +0000

    バージョンアップ

--- Flask_Kitano/calcsalary_app/salary/views.py
+++ Flask_Kitano/calcsalary_app/salary/views.py
@@ -9,48 +9,34 @@ def input():
 
 @app.route('/output', methods=['GET', 'POST'])
 def output():
+    salary = request.form['salary']
+    if len(salary) >= 10:
+        flash('給与には最大9,999,999,999まで入力可能です。')
+        return redirect(url_for('input'))
+    elif salary == "":
+        flash('給与が未入力です。入力してください。')
+        return redirect(url_for('input'))
+    
     salary = int(request.form['salary'])
-    if salary >= 1000000:
+    if salary < 0:
+        flash('給与にはマイナスの値は入力できません')
+        return redirect(url_for('input'))
+    elif salary >= 1000000:
         tax = (salary - 1000000)*0.2 + 100000
         tax = Decimal(str(tax)).quantize(Decimal("0"), rounding= ROUND_HALF_UP)
         sikyu = salary - tax
-    elif salary < 0:
-        flash('給与にはマイナスの値は入力できません')
+
+    # elif salary == "":
+    #     flash('給与が未入力です。入力してください。')
+    #     return redirect(url_for('input'))
     else:
         tax = salary * 0.1
         tax = Decimal(str(tax)).quantize(Decimal("0"), rounding= ROUND_HALF_UP)
         sikyu = salary - tax
       
-    return render_template("output.html", input_salary = salary, input_sikyu = sikyu, input_tax = tax)
-
-
-# @app.route('/logout')
-# def logout():
-#     session.pop('logged_in', None)
-#     flash('ログアウトしました')
-#     return redirect(url_for('show_entries'))
-
-
-# import sys
-# arg = sys.argv
-
-# from decimal import Decimal, ROUND_HALF_UP
-
-# salary = int(arg[1])
-
-# tax = (salary - 1000000)*0.2 + 100000
-
-
-
-# if salary >= 1000000:
-#     tax = (salary - 1000000)*0.2 + 100000
-#     tax = Decimal(str(tax)).quantize(Decimal("0"), rounding= ROUND_HALF_UP)
-#     sikyu = salary - tax
-#     print(f"支給額:{sikyu}、", end="")
-#     print(f"税額:{tax}", end="")
-# else:
-#     tax = salary * 0.1
-#     tax = Decimal(str(tax)).quantize(Decimal("0"), rounding= ROUND_HALF_UP)
-#     sikyu = salary - tax
-#     print(f"支給額:{sikyu}、", end="")
-#     print(f"税額:{tax}", end="")
+    return render_template(
+        "output.html", 
+        input_salary = salary, 
+        input_sikyu = sikyu, 
+        input_tax = tax)
+    return redirect(url_for('input'))
