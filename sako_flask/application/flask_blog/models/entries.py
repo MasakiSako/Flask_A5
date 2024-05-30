2ef592a85d446da839f3bcf0c419c5f856c03143
MasakiSako <sako.masaki.jt@mynavi.jp>
2024-05-30 04:13:19 +0000

    a

--- sako_flask/application/flask_blog/models/entries.py
+++ sako_flask/application/flask_blog/models/entries.py
@@ -1,10 +1,10 @@
 from flask_blog import db
 from datetime import datetime
-
+from zoneinfo import ZoneInfo
 class Entry(db.Model):
     __tablename__ = 'entries'
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(50),unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True)
     text = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
 
